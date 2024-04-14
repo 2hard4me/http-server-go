6faee72bbcf6632930e7d059ba4ffe771bb927f3
Abubakr_Usmanov <usmanovabubakr@gmail.com>
2024-04-14 03:34:04 +0300

    Stage4 fix

--- app/server.go
+++ app/server.go
@@ -43,7 +43,7 @@ func HandleConnection(conn net.Conn) {
 	if path == "/" {
 		response = []byte("HTTP/1.1 200 OK\r\n\r\n")
 	} else if strings.HasPrefix(path, "/echo") {
-		randStr := path[5:]
+		randStr := path[6:]
 		response = []byte("HTTP/1.1 200 OK\r\n\r\nContent-Type: text/plain\r\nContent-Length: " + strconv.Itoa(len([]rune(randStr))) + "\r\n\r\n" + randStr + "\r\n")
 	} else {
 		response = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
