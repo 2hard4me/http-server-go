92bdc8f7b41ec6203e570ccfd2bbef8f802ea751
Abubakr_Usmanov <usmanovabubakr@gmail.com>
2024-04-14 03:01:19 +0300

    Stage2

--- app/server.go
+++ app/server.go
@@ -2,17 +2,11 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -26,25 +20,24 @@ func main() {
 			os.Exit(1)
 		}
 
-		go HandleConnection(conn)
+		HandleConnection(conn)
 	}
 }
 
 func HandleConnection(conn net.Conn) {
 	defer conn.Close()
-	for {
-		buf := make([]byte, 1024)
-		_, err := conn.Read(buf)
-		if err != nil {
-			fmt.Println("Error reading:", err.Error())
-			return
-		}
-		fmt.Printf("\nRecieved data: %s", string(buf))
 
-		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-		if err != nil {
-			fmt.Println("Error writing:", err.Error())
-			return
-		}
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading data")
+		return
+	}
+
+	respOK := []byte("HTTP/1.1 200 OK\r\n\r\n")
+	_, err = conn.Write(respOK)
+	if err != nil {
+		fmt.Println("Error status OK responding")
+		return
 	}
 }
