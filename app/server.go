1f920bbb126b42cab5a55bd09ee3a4836e54bd87
Abubakr_Usmanov <usmanovabubakr@gmail.com>
2024-04-14 04:16:32 +0300

    Stage6 fix

--- app/server.go
+++ app/server.go
@@ -33,13 +33,13 @@ func HandleConnection(conn net.Conn) {
 	buf := make([]byte, 1024)
 	_, err := conn.Read(buf)
 	if err != nil {
-		fmt.Println("Error reading data")
+		fmt.Println("Error reading data", err)
 		return
 	}
 	request := string(buf)
 	status := strings.Split(request, "\r\n")
 	path := strings.Split(status[0], " ")[1]
-	userAgent := strings.Split(status[2], " ")[1]
+	
 
 	var response []byte
 	if path == "/" {
@@ -48,6 +48,7 @@ func HandleConnection(conn net.Conn) {
 		randStr := path[6:]
 		response = []byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + strconv.Itoa(len([]rune(randStr))) + "\r\n\r\n" + randStr + "\r\n")
 	} else if strings.HasPrefix(path, "/user-agent") {
+		userAgent := strings.Split(status[2], " ")[1]
 		response = []byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + strconv.Itoa(len([]rune(userAgent))) + "\r\n\r\n" + userAgent + "\r\n")
 	} else {
 		response = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
