810092d33d16780086fc10129d5d16e92e4b72fe
Abubakr_Usmanov <usmanovabubakr@gmail.com>
2024-04-14 04:03:02 +0300

    Stage6 fix

--- app/server.go
+++ app/server.go
@@ -42,7 +42,7 @@ func HandleConnection(conn net.Conn) {
 
 	var response []byte
 	if path == "/" {
-		response = []byte("HTTP/1.1 200 OK\r\n\r\n")
+		response = []byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n")
 	} else if strings.HasPrefix(path, "/echo") {
 		randStr := path[6:]
 		response = []byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + strconv.Itoa(len([]rune(randStr))) + "\r\n\r\n" + randStr + "\r\n")
